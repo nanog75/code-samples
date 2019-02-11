07b982d92cb5c1e763c6905254f9f863311a2e5d
akshshar <akshat31187@gmail.com>
2019-02-11 15:19:38 +0000

    Working setup for Tesuto topology

--- gribi/src/gribi_client/topo_delete_lsps.sh
+++ gribi/src/gribi_client/topo_delete_lsps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 gribi_client.py -j r1.gribi.json -ip localhost -port 57777 -p -d 
-python3 gribi_client.py -j r2.gribi.json -ip localhost -port 57778 -p -d
-python3 gribi_client.py -j r3.gribi.json -ip localhost -port 57779 -p -d 
-python3 gribi_client.py -j r4.gribi.json -ip localhost -port 57780 -p -d 
+python3 gribi_client.py -j r1.gribi.json -ip 100.96.0.14 -port 57777 -p -d 
+python3 gribi_client.py -j r2.gribi.json -ip 100.96.0.16 -port 57777 -p -d
+python3 gribi_client.py -j r3.gribi.json -ip 100.96.0.18 -port 57777 -p -d
+python3 gribi_client.py -j r4.gribi.json -ip 100.96.0.26 -port 57777 -p -d
