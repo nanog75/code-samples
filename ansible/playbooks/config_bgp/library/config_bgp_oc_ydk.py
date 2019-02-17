a878e48bb1a95aa6b3f9c7357b64a6649c654449
akshshar <akshat31187@gmail.com>
2019-02-17 02:11:34 +0000

    Set up the samples repo

--- ansible/playbooks/config_bgp/library/config_bgp_oc_ydk.py
+++ ansible/playbooks/config_bgp/library/config_bgp_oc_ydk.py
@@ -23,90 +23,42 @@ ANSIBLE_METADATA = {
 
 DOCUMENTATION = """
 ---
-module: ip_destination_reachable
-short_description: Verify rechability of an IP destination
-description:
-    - Uses IP ICMP echo messages to probe the reachability of an IP prefix.
-    - The module relies on GNMI to execute configure BGP on a router
-author: "Akshat Sharma (@irakshat)"
-
-options:
-    host:
-        description:
-            - IP source to send rechability probes from
-        type: str
-        required: true
-        default: null
-        choices: []
-        aliases: []
-
-    destination:
-        description:
-            - IP destination to send reachability probes to
-        type: str
-        required: true
-        default: null
-        choices: []
-        aliases: []
-
-    repeat_count:
-        description:
-            - Number of probes to test for reachability
-        type: int
-        required: false
-        default: 5
-        choices: []
-        aliases: []
-
-    vrf_name:
-        description:
-            - Name of VRF used as context for reachability test
-        type: str
-        required: false
-        default: null
-        choices: []
-        aliases: []
-
-    min_success_rate:
-        description:
-            - Percentage threshold to declare destination as reachable
-        type: int
-        required: true
-        default: null
-        choices: []
-        aliases: []
-
+module: configure_bgp_oc_ydk
 requirements:
-    - ydk 0.6.3 (python)
-    - ydk-models-cisco-ios-xr 6.3.1 (python)
+    - ydk 0.8.1 (python)
+    - ydk-models-cisco-ios-xr 6.5.1 (python)
 """
 
 EXAMPLES = """
-- ip_destination_reachable:
-    host: '10.0.0.1'
-    destination: '10.0.0.2'
-    min_success_rate: 100
-    vrf_name: 'default'
+name: configure BGP using YDK with GNMI
+      config_bgp_oc_ydk:
+        yang_repo_location: '/home/userx/yang'
+        host: "10.1.1.20"
+        grpc_port: "57777"
+        username: "rtruser"
+        password: "rtrcreds"
+        crud_op: "add"
+        bgp_params: "{{ bgp_parameters }}"
+
+where bgp_params-->
+
+bgp_parameters: {
+                       'vrf': 'default',
+                       'as': "65000",
+                       'router_id': "172.16.1.1",
+                       'peer-group-name': "IBGP",
+                       'peer-as': "65000",
+                       'peer-group-local-address': "172.16.1.1",
+                       'neighbor': "172.16.4.1"
+                      }
+
+
 """
 
 RETURN = """
-success_rate:
-  description: Percentage of successful reachability probes
-  returned: ping RPC succeeds
-  type: int
-rtt_min:
-  description: minimum round trip time of all probes
-  returned: ping RPC succeeds
-  type: int
-rtt_avg:
-  description: average round trip time of all probes
-  returned: ping RPC succeeds
-  type: int
-rtt_max:
-  description: maximum round trip time of all probes
-  returned: ping RPC succeeds
-  type: int
+  True or False based on the operation result from ydk
 """
+
 
 
 from ansible.module_utils.basic import AnsibleModule
@@ -159,48 +111,8 @@ def config_bgp_ipv4(yang_repo="",
          print("AS for BGP instance not specified")
          sys.exit(1)
 
-     if "router_id" in list(bgp.keys()):
-         protocol.bgp.global_.config.router_id = bgp["router_id"]
-
-     afi_safi = protocol.bgp.global_.afi_safis.AfiSafi()
-     afi_safi.afi_safi_name = oc_bgp_types.IPV4UNICAST()
-     afi_safi.config.afi_safi_name = oc_bgp_types.IPV4UNICAST()
-     afi_safi.config.enabled = True
-     protocol.bgp.global_.afi_safis.afi_safi.append(afi_safi)
-
-
-     if "peer-group-name" in list(bgp.keys()):
-         peer_group = protocol.bgp.peer_groups.PeerGroup()
-         peer_group.peer_group_name = bgp["peer-group-name"]
-         peer_group.config.peer_group_name = bgp["peer-group-name"]
-         if "peer-as" in list(bgp.keys()):
-             peer_group.config.peer_as = int(bgp["peer-as"])
-         if "peer-group-local-address" in list(bgp.keys()):
-             peer_group.transport.config.local_address = bgp["peer-group-local-address"]
-         
-         afi_safi = peer_group.afi_safis.AfiSafi()   
-         afi_safi.afi_safi_name = oc_bgp_types.IPV4UNICAST()
-         afi_safi.config.afi_safi_name = oc_bgp_types.IPV4UNICAST()
-         afi_safi.config.enabled = True                            
-         peer_group.afi_safis.afi_safi.append(afi_safi) 
-         protocol.bgp.peer_groups.peer_group.append(peer_group)
-
-
-     if "neighbor" in list(bgp.keys()):
-         neighbor = protocol.bgp.neighbors.Neighbor()
-         neighbor.neighbor_address = bgp["neighbor"]
-         neighbor.config.neighbor_address = bgp["neighbor"]
-         #neighbor.config.peer_as = bgp["peer-as"]
-         if "peer-group-name" in list(bgp.keys()):
-             neighbor.config.peer_group = bgp["peer-group-name"]
-
-         afi_safi = neighbor.afi_safis.AfiSafi()
-         afi_safi.afi_safi_name = oc_bgp_types.IPV4UNICAST()
-         afi_safi.config.afi_safi_name = oc_bgp_types.IPV4UNICAST()
-         afi_safi.config.enabled = True                            
-         neighbor.afi_safis.afi_safi.append(afi_safi)
-
-         protocol.bgp.neighbors.neighbor.append(neighbor)
+     # Fill out your BGP object properly using the Openconfig Yang Model
+     # You will need to bring up the IBGP neighbor between rtr1 and rtr4
 
      ni.protocols.protocol.append(protocol)
 
